ec3e0a6fa106027db18fbd3ac7e5aa009782a093
jason <jasonschermer11@gmail.com>
2022-04-05 14:14:03 +0200

    adding calculate p and q

--- rsa_code/src/main/java/com/example/PrimaryController.java
+++ rsa_code/src/main/java/com/example/PrimaryController.java
@@ -8,6 +8,7 @@ public class PrimaryController {
 
     public Text textQ;
     public Text textP;
+    String output;
 
     @FXML
     public void initialize() {
@@ -19,4 +20,18 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    String calculatePandQ(int n) {
+        int p=2;
+        while (n%p>0 && p<=n){p++;}
+        output+=p;
+        if (p<n){
+          output+=" ";
+          calculatePandQ(n);
+        }
+        
+        return output;
+    }
+
+
 }
