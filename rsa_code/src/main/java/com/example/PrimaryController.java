80697dbac997f1245b5cd2c66005c8075b71e178
Oscar Mollé <scarmoll@gmail.com>
2022-04-05 13:59:17 +0200

    P and Q interface

--- rsa_code/src/main/java/com/example/PrimaryController.java
+++ rsa_code/src/main/java/com/example/PrimaryController.java
@@ -2,8 +2,18 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class PrimaryController {
+
+    public Text textQ;
+    public Text textP;
+
+    @FXML
+    public void initialize() {
+        textQ.setText("Q goes here!");
+        textP.setText("P goes here!");
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
