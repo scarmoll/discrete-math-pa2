02485a611704ee77008a83445f11b4ef7521ade0
jason <jasonschermer11@gmail.com>
2022-04-07 13:47:42 +0200

    changing variables

--- rsa_code/src/main/java/com/example/PrimaryController.java
+++ rsa_code/src/main/java/com/example/PrimaryController.java
@@ -101,7 +101,7 @@ public class PrimaryController {
             }
         }
 
-        textE.setText(String.valueOf(e));
+        textE.setText(Integer.toString(e));
     }
 
     private int getRandomNumber(int min, int max) {
