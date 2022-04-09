18e8c7ac8bf7b490f9b8efb37bce985022314ca1
jason <jasonschermer11@gmail.com>
2022-04-09 15:03:41 +0200

    changes in screen

--- rsa_code/src/main/java/com/example/App.java
+++ rsa_code/src/main/java/com/example/App.java
@@ -14,12 +14,24 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static Scene scene1;
 
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setX(75);
+        stage.setY(100);
         stage.setScene(scene);
         stage.show();
+
+        scene1 = new Scene(loadFXML("secondary"), 640, 480);
+
+        Stage stage2 = new Stage();
+        stage2.setX(740);
+        stage2.setY(100);
+
+        stage2.setScene(scene1);
+        stage2.show();
     }
 
     static void setRoot(String fxml) throws IOException {
